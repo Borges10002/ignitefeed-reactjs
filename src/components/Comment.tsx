bfef618b3a0fcaf674ac20f880cb1017494483ec
Borges10002 <borges10002@gmail.com>
2023-10-29 16:53:30 -0400

    update

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,10 +1,15 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://avatars.githubusercontent.com/u/32522338?v=4" alt="" />
+      <Avatar
+        hasBorder={false}
+        src="https://avatars.githubusercontent.com/u/32522338?v=4"
+        alt=""
+      />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { PencilLine } from "phosphor-react";
 
 import styles from "./Sidebar.module.css";
+import { Avatar } from "./Avatar";
 
 export function Sidebar() {
   return (
@@ -11,10 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://avatars.githubusercontent.com/u/32522338?v=4"
-        />
+        <Avatar src="https://avatars.githubusercontent.com/u/32522338?v=4" />
 
         <strong>Diego Borges</strong>
         <span>Full stack Developer</span>
