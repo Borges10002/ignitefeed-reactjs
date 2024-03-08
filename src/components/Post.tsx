339598129a8d0b246f01fbfe824f0c2410389b3b
Borges10002 <borges10002@gmail.com>
2024-03-07 22:24:57 -0400

    update

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -33,7 +33,12 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event?.target.setCustomValidity("");
     setNewCommentText(event?.target.value);
+  }
+
+  function handleNewCommentInvalid() {
+    event?.target.setCustomValidity("Esse campo é obrigatário");
   }
 
   function deleteComment(commentToDelete) {
@@ -43,6 +48,8 @@ export function Post({ author, publishedAt, content }) {
 
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -84,10 +91,14 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
